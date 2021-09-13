a831ff329decc7762b2e7957a9e99e32d2607681
Ronald <ronald.cristobal.lemus@outlook.com>
2021-09-13 13:13:06 -0400

    Final Commit

--- src/Legal_Driving_Age.java
+++ src/Legal_Driving_Age.java
@@ -3,5 +3,18 @@
  *  Copyright 2021 Ronald Lemus
  */
 
+import java.util.Scanner;
+
 public class Legal_Driving_Age {
+    public static void main(String [] arg){
+        System.out.print("What is your age? ");
+        Scanner ageInp = new Scanner(System.in);
+        int age = ageInp.nextInt();
+        int legalAge = 16;
+        String result = (age >= legalAge)
+                ? "You are old enough to legally drive."
+                : "You are not old enough to legally drive.";
+        System.out.println(result);
+
+    }
 }
